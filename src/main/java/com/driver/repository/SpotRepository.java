3657bd35ce5c41e78bba1a75178dfb171770cd68
Siddheshya02 <=>
2024-03-04 20:54:28 +0530

    User and Parking updated

--- src/main/java/com/driver/repository/SpotRepository.java
+++ src/main/java/com/driver/repository/SpotRepository.java
@@ -4,7 +4,9 @@ import com.driver.model.Spot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SpotRepository extends JpaRepository<Spot, Integer>{
-
+    Optional<Spot> findById(Integer id);
 }
