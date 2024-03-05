2dbab896f4f5b0b124a4726cc65ec0e1c10d7008
Siddheshya02 <=>
2024-03-05 19:48:22 +0530

    Reservation Added

--- src/main/java/com/driver/repository/SpotRepository.java
+++ src/main/java/com/driver/repository/SpotRepository.java
@@ -1,12 +1,20 @@
 package com.driver.repository;
 
+import com.driver.model.ParkingLot;
 import com.driver.model.Spot;
+import com.driver.model.SpotType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SpotRepository extends JpaRepository<Spot, Integer>{
     Optional<Spot> findById(Integer id);
+
+    @Query(value = "SELECT s FROM Spot s WHERE s.spotType = :spotType AND s.parkingLot = :parkingLot AND s.occupied = false")
+    List<Spot> findSpotsByNumberOfWheelsAndParkingLotId(@Param("spotType") SpotType spotType,@Param("parkingLot") ParkingLot parkingLot);
 }
--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
@@ -23,7 +24,61 @@ public class ReservationServiceImpl implements ReservationService {
     ParkingLotRepository parkingLotRepository3;
     @Override
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
-        Reservation r = new Reservation();
-        return r;
+        //Exceptional Handling Remaining
+        if(numberOfWheels==4){
+            Optional<ParkingLot> park = parkingLotRepository3.findById(parkingLotId);
+            List<Spot> spotsFound = spotRepository3.findSpotsByNumberOfWheelsAndParkingLotId(SpotType.FOUR_WHEELER,park.get());
+            int minRs = Integer.MAX_VALUE;
+            int spotId = -1;
+            for(int i=0;i<spotsFound.size();i++){
+                if(minRs>spotsFound.get(i).getPricePerHour()){
+                    spotId = spotsFound.get(i).getId();
+                    minRs = spotsFound.get(i).getPricePerHour();
+                }
+            }
+            Optional<User> op = userRepository3.findById(userId);
+            Reservation reservation = new Reservation();
+            Spot spot = spotRepository3.findById(spotId).get();
+            reservation.setSpot(spot);
+            reservation.setUser(op.get());
+            reservation.setNumberOfHours(timeInHours);
+            return reservationRepository3.save(reservation);
+        }
+        else if(numberOfWheels==2){
+            Optional<ParkingLot> park = parkingLotRepository3.findById(parkingLotId);
+            List<Spot> spotsFound = spotRepository3.findSpotsByNumberOfWheelsAndParkingLotId(SpotType.TWO_WHEELER,park.get());
+            int minRs = Integer.MAX_VALUE;
+            int spotId = -1;
+            for(int i=0;i<spotsFound.size();i++){
+                if(minRs>spotsFound.get(i).getPricePerHour()){
+                    spotId = spotsFound.get(i).getId();
+                    minRs = spotsFound.get(i).getPricePerHour();
+                }
+            }
+            Optional<User> op = userRepository3.findById(userId);
+            Reservation reservation = new Reservation();
+            Spot spot = spotRepository3.findById(spotId).get();
+            reservation.setSpot(spot);
+            reservation.setUser(op.get());
+            reservation.setNumberOfHours(timeInHours);
+            return reservationRepository3.save(reservation);
+        }
+        Optional<ParkingLot> park = parkingLotRepository3.findById(parkingLotId);
+        List<Spot> spotsFound = spotRepository3.findSpotsByNumberOfWheelsAndParkingLotId(SpotType.OTHERS,park.get());
+        int minRs = Integer.MAX_VALUE;
+        int spotId = -1;
+        for(int i=0;i<spotsFound.size();i++){
+            if(minRs>spotsFound.get(i).getPricePerHour()){
+                spotId = spotsFound.get(i).getId();
+                minRs = spotsFound.get(i).getPricePerHour();
+            }
+        }
+        Optional<User> op = userRepository3.findById(userId);
+        Reservation reservation = new Reservation();
+        Spot spot = spotRepository3.findById(spotId).get();
+        reservation.setSpot(spot);
+        reservation.setUser(op.get());
+        reservation.setNumberOfHours(timeInHours);
+        return reservationRepository3.save(reservation);
     }
 }
